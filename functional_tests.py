e1fdb24bb72352a2b002759e1727ded2ae75b35b
lowderd <duncan.lowder@gmail.com>
2014-12-16 21:53:17 -0700

    First FT speccd out, now uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,13 +1,54 @@
-"""
-	This document contains functional tests
-"""
+"""This document contains functional tests"""
 
 # Imports
 
 from selenium import webdriver
+import unittest
 
-# Start  browser
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    """
+       This class defines a functional test for a NewVisitor
+    """
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_later(self):
+        """
+        Test if a to-do list can be created and then retrieved later.
+        """
+
+        # Go to apps homepage
+        self.browser.get('http://localhost:8000')
+
+        # Check page title is correct
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test')
+
+        # User enters a to-do item straight away
+
+        # She types "Buy peacock feathers" into a text box
+
+        # Hits enter, page updates, now page lists "1: Buy peacock feathers"
+        # as an item in a to-do list
+
+        # There is still a texbox to enter another item. She enters "Use
+        # peacock feathers to make a fly"
+
+        # The page updates again, now showing both items
+
+        # Edith wonders whether the site will remember the list, site generates
+        # a unique URL for the to-do list
+
+        # She visits the URL, verifies that the to-do list is still There
+
+        # Satisfied she goes back to sleep and closes the browser
+        self.browser.quit()
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
