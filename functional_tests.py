669750ab9b2562fcbd27bdbe11fa47d26541fd80
lowderd <duncan.lowder@gmail.com>
2014-12-16 23:45:17 -0700

    Added FT for to-do item indexing

--- functional_tests.py
+++ functional_tests.py
@@ -6,6 +6,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
+import time
+
 
 class NewVisitorTest(unittest.TestCase):
     """
@@ -14,10 +16,15 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
+        self.browser.implicitly_wait(1)
 
     def tearDown(self):
         self.browser.quit()
+
+    def check_for_row_in_list_tables(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_later(self):
         """
@@ -43,12 +50,16 @@ class NewVisitorTest(unittest.TestCase):
         # Hits enter, page updates, now page lists "1: Buy peacock feathers"
         # as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_tables('1: Buy peacock feathers')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table")
+        # Input second item in the to-do list
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
+        self.check_for_row_in_list_tables('1: Buy peacock feathers')
+        self.check_for_row_in_list_tables(
+            '2: Use peacock feathers to make a fly')
 
         # There is still a texbox to enter another item. She enters "Use
         # peacock feathers to make a fly"
--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,8 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.shortcuts import render
+from django.template.loader import render_to_string
 
 
 from lists.views import home_page
@@ -19,3 +21,15 @@ class HomePageTest(TestCase):
         self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn(b'<title>To-Do Lists</title>', response.content)
         self.assertTrue(response.content.endswith(b'</html>'))
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string('home.html',
+                                         {'new_item_text': 'A new list item'})
+        self.assertEqual(response.content.decode(), expected_html)
