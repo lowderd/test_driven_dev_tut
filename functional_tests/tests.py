46086f57b982a818b0c17899fb0f012c21b1c4cc
lowderd <duncan.lowder@gmail.com>
2014-12-18 21:16:06 -0700

    Changed FT to allow for test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,13 +4,27 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
+import sys
 
 
 class NewVisitorTest(StaticLiveServerTestCase):
     """
        This class defines a functional test for a NewVisitor
     """
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -30,7 +44,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         """
 
         # Go to apps homepage
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # Check page title is correct
         self.assertIn('To-Do', self.browser.title)
@@ -68,7 +82,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser = webdriver.Firefox()
 
         # Usr2 visits home-page, no sign of usr1's list
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('Use peacock feathers to make fly', page_text)
@@ -90,7 +104,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         # Usr1 goes to the homepage
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         # Check that input box is centered
