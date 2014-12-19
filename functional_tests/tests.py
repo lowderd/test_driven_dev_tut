6406d39e85dad851c5096761190839e93547e36e
lowderd <duncan.lowder@gmail.com>
2014-12-18 20:47:30 -0700

    Use bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,13 +1,13 @@
 """This document contains functional tests"""
 
 # Imports
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     """
        This class defines a functional test for a NewVisitor
     """
